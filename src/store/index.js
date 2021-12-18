f6a6ee0eed5084b21162a9b6ef73e24d6efac995
uzunvictor <v3i3c3@gmail.com>
2021-12-18 21:00:28 +0200

    sorry, but this is my first commit)))

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import auth from '@/store/modules/auth';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
+  state: {count: 0},
   mutations: {
+    increment(state) {
+      state.count++;
+    },
   },
-  actions: {
-  },
+  actions: {},
   modules: {
-  }
-})
+    auth,
+  },
+});
