f6183ce63dfbec6d3ba021d44e93573ad6f86424
uzunvictor <v3i3c3@gmail.com>
2021-12-22 11:01:21 +0200

    creating test component and its path

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/views/Home.vue';
 import Register from '@/views/Register.vue';
+import Test from '@/views/Test.vue';
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/register',
     name: 'register',
     component: Register,
+  },
+  {
+    path: '/test',
+    name: 'test',
+    component: Test,
   },
 ];
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,63 +1,9 @@
 <template>
-  <div class="home">
-    Home page
-    <button @click="firstTestMutation">First</button>
-    <button @click="secondTestMutation">Second</button>
-    <button @click="thirdTestMutation">Third</button>
-    <h1>first: {{ firstTestElement }}</h1>
-    <h1>second: {{ secondTestElement }}</h1>
-    <h1>third: {{ thirdTestElement }}</h1>
-    <br />
-    <h3>third: {{ sumOfTestElements }}</h3>
-    <button @click="gettingSum">Third</button>
-    <h3>The result from action: {{ sumFromAction }}</h3>
-  </div>
+  <div class="home">Home page</div>
 </template>
 
 <script>
-import {testTypesMutation} from '@/store/modules/test';
 export default {
-  data() {
-    return {
-      sumFromAction: 0,
-    };
-  },
-
   name: 'Home',
-
-  computed: {
-    firstTestElement() {
-      return this.$store.state.test.first;
-    },
-    secondTestElement() {
-      return this.$store.state.test.second;
-    },
-    thirdTestElement() {
-      return this.$store.state.test.third;
-    },
-    sumOfTestElements() {
-      return this.$store.state.test.sum;
-    },
-  },
-
-  methods: {
-    firstTestMutation() {
-      this.$store.commit(testTypesMutation.changeFirst);
-    },
-    secondTestMutation() {
-      this.$store.commit(testTypesMutation.changeSecond, this.firstTestElement);
-    },
-    thirdTestMutation() {
-      this.$store.commit('[test] changeThird');
-    },
-    gettingSum() {
-      this.$store.dispatch('getSumOfElements').then((result) => {
-        this.sumFromAction = result;
-        setTimeout(() => {
-          this.$router.push({name: 'register'});
-        }, 2000);
-      });
-    },
-  },
 };
 </script>
